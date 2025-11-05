c6ab60c72dd7d4f8fcc9545550eb137d3dc8415d
wilson2533 <wilsonminaya23062003@gmail.com>
2025-11-05 16:07:20 -0400

    Update 69xinshu.ts

--- plugins/chinese/69xinshu.ts
+++ plugins/chinese/69xinshu.ts
@@ -10,7 +10,7 @@ class XinShu69 implements Plugin.PluginBase {
   name = '69书吧';
   icon = 'src/cn/69xinshu/icon.png';
   site = 'https://www.69shuba.com/';
-  version = '0.1.4';
+  version = '0.1.2';
 
   async popularNovels(
     pageNo: number,
